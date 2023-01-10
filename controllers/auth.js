a5703ff4cb2e5585b2b419eeaaaab7abd0d32b86
EbukaNweje <nwejeebuka@gmail.com>
2023-01-10 16:14:49 +0100

    Register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,31 @@
 const user = require('../models/AddAccount')
 const bcrypt = require("bcryptjs");
 const createError = require("../utilities/error");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
+
+exports.register = async (req, res, next)=>{
+    try {
+        const {email} = req.body
+        user.findOne({email}) = async (err, User) =>{
+            if(err){
+                res.status(400)
+            }
+            if(User){
+            return  res.status(200).json({Message: "Email already in use"})
+            }else if(!User){
+                const addUser = await user.create(req.body)
+               return res.status(201).json({
+                    Message: 'user created successfullY!',
+                    data : addUser
+                })
+            }
+            else{
+                return next()
+            }
+        }
+
+    }catch(err){
+        next(err)
+    }
+}
